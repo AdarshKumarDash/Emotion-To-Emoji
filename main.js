2626aa0514573c0893a25b0153b2b518bcee065b
Adarsh Kumar <86519736+AdarshKumarDash@users.noreply.github.com>
2022-03-30 19:34:21 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 //https://teachablemachine.withgoogle.com/models/6B5uOW5Dy/
-
 speak_data1 = "";
 speak_data2 = "";
 
@@ -30,4 +29,36 @@ function speech() {
     data2 = "The second prediction is " + speak_data2;
     var utterance = new SpeechSynthesisUtterance(data1 + data2);
     synth.speak(utterance);
+}
+
+function identify() {
+    img = document.getElementById("captured_image");
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, result) {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log(result);
+        speak_data1 = result[0].label;
+        speak_data2 = result[1].label;
+        document.getElementById("emotion1").innerHTML = speak_data1;
+        document.getElementById("emotion2").innerHTML = speak_data2;
+        speech();
+        if (speak_data1 == "Happy") {
+            document.getElementById("updateemoji1").innerHTML = "&#128522;";
+        } else if (speak_data1 == "Sad") {
+            document.getElementById("updateemoji1").innerHTML = "&#128532;";
+        } else if (speak_data1 == "Angry") {
+            document.getElementById("updateemoji1").innerHTML = "&#128545;";
+        }
+        if (speak_data2 == "Happy") {
+            document.getElementById("updateemoji2").innerHTML = "&#128522;";
+        } else if (speak_data2 == "Sad") {
+            document.getElementById("updateemoji2").innerHTML = "&#128532;";
+        } else if (speak_data2 == "Angry") {
+            document.getElementById("updateemoji2").innerHTML = "&#128545;";
+        }
+    }
 }
